0947b8917b3434e1b6c544f659ae74f57abce5f1
yago-carvalho <yagocarvalho.dev@gmail.com>
2024-09-12 15:16:54 -0300

    add open goals

--- index.js
+++ index.js
@@ -59,8 +59,24 @@ const metasRealizadas = async () => {
     }
 
     await select({
-        message: "Metas realizadas",
+        message: "Metas realizadas " + realizadas.length,
         choices: [...realizadas]
+    })
+}
+
+const metasAbertas = async () => {
+    const abertas = metas.filter((meta) => {
+        return !meta.checked
+    })
+
+    if(abertas.length == 0) {
+        console.log("Não existem metas abertas! :)")
+        return 
+    }
+
+    await select({
+        message: "Metas abertas " + abertas.length,
+        choices: [...abertas]
     })
 }
 
@@ -84,6 +100,10 @@ const start = async () => {
                 value: "realizadas"
             },
             {
+                name: "Metas abertas",
+                value: "abertas"
+            },
+            {
                 name: "Sair",
                 value: "sair"
             }
@@ -100,6 +120,9 @@ const start = async () => {
                 break
             case "realizadas":
                 await metasRealizadas()
+                break
+            case "abertas":
+                await metasAbertas()
                 break
             case "sair":
                 console.log("Até a próxima!")
--- todo.md
+++ todo.md
@@ -1,7 +1,7 @@
 [x] Criar metas
 [x] Listar metas
     [x] Metas realizadas
-    - Metas abertas
+    [x] Metas abertas
 [x] Marcar/Desmarcar metas realizadas
 [x] Remover metas
 [x] Sistema de mensagens
